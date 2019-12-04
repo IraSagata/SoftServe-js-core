07d343a204a905a93181618bcf0bd3eb2b7da920
IraSagata <irasagata@gmail.com>
2019-12-04 08:53:56 +0200

    jquery exercises

---
+++ Homework_9/script.js
@@ -0,0 +1,21 @@
+// //task-1
+$("a[href^='http://']").attr("target", "_blank");
+
+// //task-2
+// $(".head").css("background-color", "green");
+// $(".inner").css("font-size", "35px");
+
+// //task-3
+// $("#element1")
+//   .detach()
+//   .prependTo("#header1");
+// $("#element2")
+//   .detach()
+//   .prependTo("#header2");
+
+// //task-4
+// $("input").click(function() {
+//   if ($(this).is(":checked")) {
+//     $("input").attr("disabled", true);
+//   }
+// });
